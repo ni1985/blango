e214e5bebc3198de3d91e74c208762afc61f0bb6
Kirill Nikel <ni1985@mail.ru>
2022-10-03 20:33:15 +0000

    Finish custom template tags

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -2,10 +2,12 @@ from django.contrib.auth import get_user_model
 from django.utils.html import escape, format_html
 from django.utils.safestring import mark_safe
 from django import template
+from blog.models import Post
 
 register = template.Library()
 user_model = get_user_model()
 
+# Implementation with filters and arguments
 @register.filter
 def author_details(author, current_user=None):
     if not isinstance(author, user_model):
@@ -28,3 +30,56 @@ def author_details(author, current_user=None):
         suffix = ""
 
     return format_html("{}{}{}", prefix, name, suffix)
+
+# Register simple tags
+@register.simple_tag
+def row(extra_classes=""):
+    return format_html('<div class="row {}">', extra_classes)
+
+@register.simple_tag
+def endrow():
+    return format_html("</div>")
+
+@register.simple_tag
+def col(extra_classes=""):
+    return format_html('<div class="row {}">', extra_classes)
+
+@register.simple_tag
+def endcol():
+    return format_html("</div>")
+
+# Register Inclusion tags
+@register.inclusion_tag("blog/post-list.html")
+def recent_posts(post):
+    posts = Post.objects.exclude(pk=post.pk)[:5]
+    return {"title": "Recent Posts", "posts": posts}
+
+
+"""
+# implementation as a template tag without arguments
+@register.simple_tag(takes_context=True)
+def author_details_tag(context):
+    request = context["request"]
+    current_user = request.user
+    post = context["post"]
+    author = post.author
+
+    if author == current_user:
+        return format_html("<strong>me</strong>")
+
+    if author.first_name and author.last_name:
+        name = f"{author.first_name} {author.last_name}"
+    else:
+        name = f"{author.username}"
+
+    if author.email:
+        prefix = format_html('<a href="mailto:{}">', author.email)
+        suffix = format_html("</a>")
+    else:
+        prefix = ""
+        suffix = ""
+
+    return format_html("{}{}{}", prefix, name, suffix)
+# end of example
+"""
+
