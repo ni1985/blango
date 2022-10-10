fe0e80c0c50d55ab15720ccc8dbea43f906ee532
Kirill Nikel <ni1985@mail.ru>
2022-10-10 19:48:31 +0000

    Finish caching

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -3,8 +3,10 @@ from django.utils.html import escape, format_html
 from django.utils.safestring import mark_safe
 from django import template
 from blog.models import Post
+import logging
 
 register = template.Library()
+logger = logging.getLogger(__name__)
 user_model = get_user_model()
 
 # Implementation with filters and arguments
@@ -52,6 +54,7 @@ def endcol():
 @register.inclusion_tag("blog/post-list.html")
 def recent_posts(post):
     posts = Post.objects.exclude(pk=post.pk)[:5]
+    logger.debug("Loaded %d recent posts for post %d", len(posts), post.pk)
     return {"title": "Recent Posts", "posts": posts}
 
 
