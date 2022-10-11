bbc7c7252cdaa0f7c9c9e6a9595eaaa9c7064182
Kirill Nikel <ni1985@mail.ru>
2022-10-11 05:40:04 +0000

    Finish database optimization

--- blango/settings.py
+++ blango/settings.py
@@ -54,10 +54,12 @@ class Dev(Configuration):
         'django.contrib.staticfiles',
         'blog',
         'crispy_forms',
-        'crispy_bootstrap5'
+        'crispy_bootstrap5',
+        "debug_toolbar",
     ]
 
     MIDDLEWARE = [
+        "debug_toolbar.middleware.DebugToolbarMiddleware",
         'django.middleware.security.SecurityMiddleware',
         'django.contrib.sessions.middleware.SessionMiddleware',
         'django.middleware.common.CommonMiddleware',
@@ -134,6 +136,7 @@ class Dev(Configuration):
 
     USE_TZ = True
 
+    INTERNAL_IPS = ["192.168.10.226"]
 
     # Static files (CSS, JavaScript, Images)
     # https://docs.djangoproject.com/en/3.2/howto/static-files/
